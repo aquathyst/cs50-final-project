6d32b2d5d2bae94f434a68166f86c6c8bb833c41
Peter Wang <ptrw08@gmail.com>
2015-11-28 12:26:35 -0500

    First stab at js (ptrw08)

---
+++ contentscript.js
@@ -0,0 +1,7 @@
+/*
+  AlphaText
+  Content Scipt
+*/
+
+
+
--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
+/*
+  AlphaText
+  JavaScript for popup.html
+*/
+
 function click(e) {
 
   chrome.tabs.executeScript(null,
@@ -20,16 +25,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-var onoff='off';
+var onoff='false';
+
+function showonofftext(){
+	if(onoff='true')
+	{
+		document.getElementById('onofftext').innerHTML='Switch off';
+	}
+	else
+	{
+		document.getElementById('onofftext').innerHTML='Switch on';
+	}
+}
 
 /* Swtich on or off */
 function switch(){
 	if(onoff==='on'){
 		onoff='off';
+		
+		document.getElementById('on').id='off';
+
 	}
 	else{
 		onoff='on';
-
+		document.getElementById('off').id='on';
 	}
 }
 
@@ -47,4 +66,38 @@ function adoptp(profile){
 		default:
 		;
 	}
+}
+
+var saved=false;
+
+function checksave(domain){
+	
+	if(saved=true)
+	{
+		document.getElementById('savepage').onclick='unsave()';
+		document.getElementById('savepage').id='unsavepage';
+	}
+}
+
+function showsavetext(){
+	if(saved='true')
+	{
+		document.getElementById('savetext').innerHTML='Don\'t always use on domain';
+	}
+	else
+	{
+		document.getElementById('savetext').innerHTML='Always use on domain';
+	}
+}
+
+function save(domain,profile){
+	document.getElementById('savepage').onclick='unsave()';
+	document.getElementById('savepage').id='unsavepage';
+
+}
+
+function unsave(domain){
+	document.getElementById('unsaveoage').onclick='save()';
+	document.getElementById('unsavepage').id='savepage';
+	
 }

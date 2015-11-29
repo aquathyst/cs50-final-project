b0b2009e35cec760ed88d8c949bb7d85ae071099
Peter Wang <ptrw08@gmail.com>
2015-11-29 13:57:25 -0500

    JS and building functionality
    toggle, renderstatus, adoptp now at least partially functional.
    button --> qsset

--- popup.js
+++ popup.js
@@ -3,30 +3,107 @@
   JavaScript for popup.html
 */
 
+/* Variables to track state */
+var onoff=false;
+var activep=null;
 
-function click(e) {
+/* Render status text */
+function renderstatus(eventt){
+	switch(eventt){
+		case 'adoptp':
+			document.getElementById("status").innerHTML='Profile '+activep+' activated!';
+			break;
+		case 'error':
+			document.getElementById("status").innerHTML='Error!';
+			break;
+		case 'save':
+			document.getElementById("status").innerHTML='Domain saved.';
+			break;
+		case 'unsave':
+			document.getElementById("status").innerHTML='Domain save removed.';
+			break;
+		case 'quickstyleset':
+			document.getElementById("status").innerHTML='Quick style set successfully!';
+			break;
+		default:
+			document.getElementById("status").innerHTML=eventt;
+			break;
+	}
+}
+
+/* Toggle On-Off */
+function toggle(e){
+	if(onoff===false){
+		// Switch on
+		onoff=true;
+		document.getElementById("onofftext").innerHTML='Switch off';
+		document.getElementById("off").id='on';
+		adoptp('default');
+	}
+	else{
+		// Switch off
+		onoff=false;
+		document.getElementById("onofftext").innerHTML='Switch on';
+		document.getElementById("on").id='off';
+		//  TODO: Remove all injected styles
+	}
+}
+
+/* Quick Styles */
+function qsset(e) {
   var fsize = document.getElementById("font_size").value;
   var ffamily = document.getElementById("font_family").value;
+  var lheight = document.getElementById("line_height").value;
   chrome.tabs.executeScript(null,
     {code:"document.body.style.fontSize = '" + fsize + "'"});
   chrome.tabs.executeScript(null,
     {code:"document.body.style.fontFamily = '" + ffamily + "'"});
-  window.close();
+  chrome.tabs.executeScript(null,
+    {code:"document.body.style.lineHeight = '" + lheight + "'"});
+  //window.close();
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+/* Adapt and inject a style profile */
+function adoptp(profile){
+	
+	// TODO: Remove all injected css and styles
+	
+	switch(profile){
+		case 'default':
+			chrome.tabs.insertCSS(null,{file:"profiles/default.css"});
+			break;
+		// case 1:
+		// 	chrome.tabs.insertCSS(null,{file:"profiles/user1.css"});
+		// 	break;
+		// case 2:
+		// 	chrome.tabs.insertCSS(null,{file:"profiles/user2.css"});
+		// 	break;
+		// case 3:
+		// 	chrome.tabs.insertCSS(null,{file:"profiles/user3.css"});
+		// 	break;
+		default:
+			return;
+	}
+	activep=profile;
+	renderstatus('adoptp');
+}
+
+/* Button event listeners */
+document.addEventListener('DOMContentLoaded',function(){
+  // Quick Style Set
   var button = document.querySelectorAll("#setstyle");
   for (var i = 0, len = button.length; i < len; i++) {
-    button[i].addEventListener('click', click);
+    button[i].addEventListener('click', qsset);
+  }
+  // On-off toggle
+  var onofftoggle = document.querySelectorAll("#off");
+  for (var i = 0, len = onofftoggle.length; i < len; i++) {
+    onofftoggle[i].addEventListener('click', toggle);
   }
 });
 
 
 
-/* On-off switch scripts */
-var onoff=false;
-
-var activep=null;
 
 // function checkonoff(){
 // 	if(onoff===true)
@@ -47,62 +124,12 @@ var activep=null;
 // 	}
 // }
 
-function toggle(){
-	if(onoff===true){
-		// Switch off
-		onoff=false;
-		$("#onofftext").innerHTML='Switch on';
-		$("#on").id='off';
-		//  TODO: Remove all injected styles
-	}
-	else{
-		// Switch on
-		onoff=true;
-		$("#onofftext").innerHTML='Switch off';
-		$("#off").id='on';
-		adoptp('default');
-	}
-}
 
 
-/* Adapt and inject a style profile */
-function adoptp(profile){
-	
-	// TODO: Remove all injected css and styles
-	
-	switch(profile){
-		case 'default':
-		chrome.tabs.insertCSS(null,{file:"profiles/default.css"});
-		case 1:
-		chrome.tabs.insertCSS(null,{file:"profiles/user1.css"});
-		case 2:
-		chrome.tabs.insertCSS(null,{file:"profiles/user2.css"});
-		case 3:
-		chrome.tabs.insertCSS(null,{file:"profiles/user3.css"});
-		default:
-		return;
-	}
-	activep=profile;
-	renderstatus('adoptp');
-}
 
-/* Render status text */
-function renderstatus(event){
-	switch(event){
-		case 'adoptp':
-		$("#status").innerHTML='Profile '+profile+' activated!';
-		case 'error':
-		$("#status").innerHTML='Error!';
-		case 'save':
-		$("#status").innerHTML='Domain saved.';
-		case 'unsave':
-		$("#status").innerHTML='Domain save removed.';
-		case 'quickstyleset':
-		$("#status").innerHTML='Quick style set successfully!';
-		default:
-		;
-	}
-}
+
+
+
 
 
 // /* Get url and domain of website */
